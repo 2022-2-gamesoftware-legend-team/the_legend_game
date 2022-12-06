f3f7705657d74906e02128a4d3a0c1843e10b415
parkjongheum <parkjh0821@gmail.com>
2022-12-07 00:22:02 +0900

    Feat_multiplayer: Finish 감지 Scene 변경 코드 추가

---
+++ Assets/Scripts/PlayerSceneChange.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerSceneChange : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ServerCallback]
+    void OnTriggerEnter2D(Collider2D collider) {
+        GameNetworkManager gameNetworkManager = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<GameNetworkManager>();
+        if (gameNetworkManager) {
+            gameNetworkManager.NextStage();
+        } else {
+            print("Error: GameNetworkManager not found.");
+        }
+    }
+}
